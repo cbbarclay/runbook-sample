a9089ef048445c6a44ae0cc9d05f985726def303
cbbarclay <cbbarclay@hotmail.com>
2014-03-17 14:39:44 -0700

    Update testdatabag.rb

--- layer-custom/recipes/testdatabag.rb
+++ layer-custom/recipes/testdatabag.rb
@@ -1,3 +1,3 @@
 mything = data_bag_item("myapp", "mysql")
 #attribute_i_want = mything[node.chef_environment]
-Chef::Log.debug("The values are #{attribute_i_want} ")
+Chef::Log.debug("The values are #{mything} ")
