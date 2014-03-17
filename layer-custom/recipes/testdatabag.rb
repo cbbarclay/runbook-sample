89bbfa3e6633c7a8820e5aefb4ff00be413facf1
cbbarclay <cbbarclay@hotmail.com>
2014-03-17 15:20:41 -0700

    Update testdatabag.rb

--- layer-custom/recipes/testdatabag.rb
+++ layer-custom/recipes/testdatabag.rb
@@ -2,3 +2,6 @@ mything = data_bag_item("myapp", "mysql")
 #attribute_i_want = mything[node.chef_environment]
 Chef::Log.debug("The values are #{mything} ")
 Chef::Log.debug("The values are '#{mything[node.chef_environment]['username']}' ")
+
+appserver = search(:node, "role:php-app").first
+Chef::Log.debug("The private IP is '#{appserver[:private_ip]}'")
