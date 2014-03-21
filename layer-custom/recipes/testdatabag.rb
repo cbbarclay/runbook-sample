cdbad56c305fc5e51e1299bd3ba3f42a2e5e7d5f
cbbarclay <cbbarclay@hotmail.com>
2014-03-21 10:48:26 -0700

    Update testdatabag.rb

--- layer-custom/recipes/testdatabag.rb
+++ layer-custom/recipes/testdatabag.rb
@@ -1,7 +1,7 @@
 mything = data_bag_item("myapp", "mysql")
 #attribute_i_want = mything[node.chef_environment]
 #Chef::Log.debug("The values are #{mything} ")
-Chef::Log.debug("The username is '#{mything['default']['username']}' ")
+Chef::Log.debug("The username is '#{mything[node.chef_environment]['username']}' ")
 #Chef::Log.debug("The values are '#{mything['username']}' ")
 
 appserver = search(:node, "role:php-app").first
